84a55b1f82e9b313442378bed25e9d7efea563cc
Thai Nguyen <quocthai14052004@gmail.com>
2024-11-21 16:27:09 -0500

    Update project.c

--- project.c
+++ project.c
@@ -161,7 +161,14 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-    int values = 0xffff << 16; *extended_value = (offset & 1 << 15 ? offset | values : offset);
+     unsigned signBit = offset >> 15; /* the sign but is the left most bit of the 16 bit string */
+    if(signBit == 1) {
+        /* negative, expand to 32 bits */
+        *extended_value = offset | 0xFFFF0000; /* left half are all 1's */
+    } else {
+        /* not negative, just keep at 16 bits */
+        *extended_value = offset & 0x0000ffff;
+    }
 }
 
 /* ALU operations */
